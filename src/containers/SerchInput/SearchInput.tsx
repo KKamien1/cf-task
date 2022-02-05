1e467e101b2ee1dbcaa3abc3eede42d9e193bcf9
Kris <kamienieckik@gmail.com>
2022-02-05 18:18:32 +0100

    component: SearchInput reset button added

--- src/containers/SerchInput/SearchInput.tsx
+++ src/containers/SerchInput/SearchInput.tsx
@@ -1,23 +1,33 @@
-import React, { FormEvent } from 'react';
-import Title from '../../components/Title/Title';
+import React, { FormEvent, MouseEvent } from 'react';
 import useInput from '../../hooks/useInput';
-function SearchInput() {
-    const [Input, keyword, resetKeyword] = useInput();
+
+type Props = {
+    setKeyword: (value: string) => void;
+};
+
+function SearchInput({ setKeyword }: Props) {
+    const [Input, value, resetValue] = useInput();
+    const isValid = value.length > 0;
 
     const submitHandler = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (keyword.length > 0) {
-            resetKeyword();
+        if (isValid) {
+            setKeyword(value);
         }
     };
+
+    const resetHandler = (e: MouseEvent<HTMLElement>) => resetValue();
+
     return (
-        <>
-            <Title>Search for public API</Title>
-            <form onSubmit={submitHandler}>
-                {Input}
-                <button type='submit'>Search</button>
-            </form>
-        </>
+        <form onSubmit={submitHandler}>
+            {Input}
+            <button type='submit'>Search</button>
+            {isValid && (
+                <button type='reset' onClick={resetHandler}>
+                    Reset
+                </button>
+            )}
+        </form>
     );
 }
 
