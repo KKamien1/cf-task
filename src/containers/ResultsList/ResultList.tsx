8bbc4d648fe8b2e305820f818b8e764f335f8ae6
Kris <kamienieckik@gmail.com>
2022-02-05 01:39:49 +0100

    setup: uuid added

--- src/containers/ResultsList/ResultList.tsx
+++ src/containers/ResultsList/ResultList.tsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import ListItem from '../../components/ListItem/ListItem'
-import { AssetType } from '../../components/ListItem/types'
-import Title from '../../components/Title/Title'
+import React from 'react';
+import ListItem from '../../components/ListItem/ListItem';
+import { AssetType } from '../../components/ListItem/types';
+import Title from '../../components/Title/Title';
+import { v4 as uuidv4 } from 'uuid';
 
 type Props = {
-    results: AssetType[]
-}
+    results: AssetType[];
+};
 
 export default function ResultList({ results = [] }: Props) {
-    console.log('first', results)
+    console.log('first', results);
     return (
         <div>
             <Title>Results</Title>
             {results.map((item) => (
-                <ListItem key={item.API} item={item} />
+                <ListItem key={uuidv4()} item={item} />
             ))}
         </div>
-    )
+    );
 }
